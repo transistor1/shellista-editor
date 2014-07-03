08a0e08761f20a039bb9f470078719f980622360
Steve Russo <svrusso1@gmail.com>
2014-07-03 17:13:33 +0000

    Open file using full path

--- editor_plugin.py
+++ editor_plugin.py
@@ -1,6 +1,9 @@
 '''Open a file in the Pythonista text editor, or vi
-if not on iPad
+if not on iPad. Only works with file extensions that
+Pythonista recognizes.
 '''
+
+import os
 
 HAS_EDITOR = False
 
@@ -8,17 +11,17 @@ try:
     import editor
     HAS_EDITOR = True
 except:
-    import os
+    pass
 
 from ... tools.toolbox import bash
 
 def main(self, line):
-    print line
     args = bash(line)
-
+    
     if len(args) == 1:
         if HAS_EDITOR:
-            editor.open_file(args[0])
+            print 'Opening {0} in Pythonista editor.'.format(args[0])
+            editor.open_file(os.path.join(os.getcwd(), args[0]))
         else:
             import platform
             if platform.system() == 'Linux':
